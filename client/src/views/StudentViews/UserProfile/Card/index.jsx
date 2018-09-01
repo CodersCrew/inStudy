3aad7222f35c704ca377718e3978c1786786fec2
Konrad Szwarc <konrad.szwarc@outlook.com>
2018-09-01 17:42:09 +0200

    Add recompose

--- client/src/views/StudentViews/UserProfile/Card/index.jsx
+++ client/src/views/StudentViews/UserProfile/Card/index.jsx
@@ -1,56 +1,50 @@
-import React, { PureComponent } from 'react';
-import { string, arrayOf, object } from 'prop-types';
-import { pick } from 'utils';
+import React from 'react';
+import { string, arrayOf, object, bool, func } from 'prop-types';
+import { compose, withState, withHandlers } from 'recompose';
 import { socials } from 'data';
 import CardEditModal from './CardEditModal';
-import { Container, Image, Name, Description, Socials, Social, EditIcon } from './styles';
+import { Container, Image, Name, Description, Socials, SocialItem, EditIcon } from './styles';
 
-const renderSocial = ({ type, url }) => (
-  <Social color={socials[type].color} href={url} className={socials[type].icon} />
-);
+const Social = ({ type, url }) => <SocialItem color={socials[type].color} href={url} className={socials[type].icon} />;
 
-renderSocial.propTypes = {
+Social.propTypes = {
   type: string.isRequired,
   url: string.isRequired,
 };
 
-const userDataToPick = ['firstName', 'lastName', 'image', 'email', 'description', 'socials'];
+const withHocs = compose(
+  withState('isModalOpen', 'setModal', false),
+  withHandlers({
+    openModal: ({ setModal }) => () => setModal(() => true),
+    closeModal: ({ setModal }) => () => setModal(() => false),
+  }),
+);
 
-class Card extends PureComponent {
-  state = {
-    isModalOpen: false,
-  };
-
-  openModal = () => this.setState({ isModalOpen: true });
-
-  closeModal = () => this.setState({ isModalOpen: false });
-
-  render() {
-    const { image, firstName, lastName, description, socials } = this.props;
-
-    return (
-      <Container>
-        <Image src={image} />
-        <Name>{`${firstName} ${lastName}`}</Name>
-        <Description>{description}</Description>
-        {socials.length > 0 && <Socials>{socials.map(renderSocial)}</Socials>}
-        <EditIcon className="fal fa-edit" onClick={this.openModal} />
-        <CardEditModal
-          visible={this.state.isModalOpen}
-          onClose={this.closeModal}
-          userData={pick(this.props, userDataToPick)}
-        />
-      </Container>
-    );
-  }
-}
+const Card = ({ firstName, lastName, description, image, email, socials, isModalOpen, openModal, closeModal }) => (
+  <Container>
+    <Image src={image} />
+    <Name>{`${firstName} ${lastName}`}</Name>
+    <Description>{description}</Description>
+    {socials.length > 0 && <Socials>{socials.map(Social)}</Socials>}
+    <EditIcon className="fal fa-edit" onClick={openModal} />
+    <CardEditModal
+      visible={isModalOpen}
+      onClose={closeModal}
+      userData={{ firstName, lastName, image, email, description, socials }}
+    />
+  </Container>
+);
 
 Card.propTypes = {
+  email: string.isRequired,
   image: string.isRequired,
   firstName: string.isRequired,
   lastName: string.isRequired,
   description: string,
   socials: arrayOf(object),
+  isModalOpen: bool.isRequired,
+  openModal: func.isRequired,
+  closeModal: func.isRequired,
 };
 
 Card.defaultProps = {
@@ -58,4 +52,4 @@ Card.defaultProps = {
   socials: [],
 };
 
-export default Card;
+export default withHocs(Card);
